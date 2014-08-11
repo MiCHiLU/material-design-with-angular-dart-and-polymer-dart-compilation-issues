5de39f3b288a7a50ed8c201e8680d4f7b8d24f21
ENDOH takanao <djmchl@gmail.com>
2014-08-11 17:33:33 +0800

    initial import
    http://stackoverflow.com/questions/24723315/material-design-with-angular-dart-and-polymer-dart-compilation-issues

---
+++ web/Application.dart
@@ -0,0 +1,15 @@
+import 'package:polymer/polymer.dart';
+import 'package:angular/angular.dart';
+import 'package:angular/application_factory.dart';
+import 'package:angular_node_bind/angular_node_bind.dart';
+
+// HACK until we fix code gen size. This doesn't really fix it,
+// just makes it better.
+@MirrorsUsed(override: '*')
+import 'dart:mirrors';
+
+void main() {
+  initPolymer().run(() {
+    applicationFactory().addModule(new AppModule()).addMdoule(new NodeBindModule()).run();
+  });
+}
